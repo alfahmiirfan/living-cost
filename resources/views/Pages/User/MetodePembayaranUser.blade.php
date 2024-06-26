84157a6787b82c2d132fe7d79b18a3a1e14e4304
Alfahmi Irfan <alfahmiirfan11@gmail.com>
2024-06-26 17:36:34 +0700

    update-select bank option

--- resources/views/Pages/User/MetodePembayaranUser.blade.php
+++ resources/views/Pages/User/MetodePembayaranUser.blade.php
@@ -5,6 +5,17 @@
     <title>Metode Pembayaran</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
+    <style>
+        .bank-option {
+            cursor: pointer;
+            transition: all 0.3s ease;
+        }
+
+        .bank-option:hover, .bank-option.active {
+            border-color: #86B6F6;
+            background-color: #B4D4FF;
+        }
+    </style>
 </head>
 <body>
     <x-layout-user>
@@ -26,35 +37,35 @@
         <div class="wrap">
             <div class="wrap-pay">
                 <div class="pb-2">
-                    <a href="/VirtualAccountUser" class="bank-option" data-bank="BANK BRI" data-image="image/bri.svg">
+                    <div class="bank-option" data-bank="BANK BRI" data-image="image/bri.svg">
                         <div class="border-2 flex h-18 w-full col-span-1 border-gray-300 rounded-md">
                             <div class="p-4 flex">
                                 <img src="image/bri.svg" class="w-10 h-10">
                                 <h1 class="pt-1 pl-4 font-bold text-xl">BANK BRI</h1>
                             </div>
                         </div>
-                    </a>
+                    </div>
                 </div>
                 <div class="pb-2">
-                    <a href="/VirtualAccountUser" class="bank-option" data-bank="BANK BNI" data-image="image/bni.svg">
+                    <div class="bank-option" data-bank="BANK BNI" data-image="image/bni.svg">
                         <div class="border-2 flex h-18 w-full col-span-1 border-gray-300 rounded-md">
                             <div class="p-4 flex">
                                 <img src="image/bni.svg" class="w-10 h-10">
                                 <h1 class="pt-1 pl-4 font-bold text-xl">BANK BNI</h1>
                             </div>
                         </div>
-                    </a>
+                    </div>
                 </div>
-            </div>
-            <div class="pb-2">
-                <a href="/VirtualAccountUser" class="bank-option" data-bank="BANK BCA" data-image="image/bca.svg">
-                    <div class="border-2 flex h-18 w-full col-span-1 border-gray-300 rounded-md">
-                        <div class="p-4 flex">
-                            <img src="image/bca.svg" class="w-10 h-10">
-                            <h1 class="pt-1 pl-4 font-bold text-xl">BANK BCA</h1>
+                <div class="pb-2">
+                    <div class="bank-option" data-bank="BANK BCA" data-image="image/bca.svg">
+                        <div class="border-2 flex h-18 w-full col-span-1 border-gray-300 rounded-md">
+                            <div class="p-4 flex">
+                                <img src="image/bca.svg" class="w-10 h-10">
+                                <h1 class="pt-1 pl-4 font-bold text-xl">BANK BCA</h1>
+                            </div>
                         </div>
                     </div>
-                </a>
+                </div>
             </div>
         </div>
         <div class="">
@@ -67,6 +78,13 @@
                 </div>
             </div>
         </div>
+        <div class="pt-4 flex float-right">
+            <a href="#" id="pay-now">
+                <button type="button" class="font-bold px-5 py-3 text-sm  text-black inline-flex items-center bg-[#F5E81D] hover:bg-yellow-300 focus:ring-4 focus:outline-none focus:ring-yellow-400 rounded-lg text-center dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800">
+                    Bayar Sekarang
+                </button>
+            </a>
+        </div>
         <x-tab-user />
     </x-layout-user>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
@@ -74,10 +92,16 @@
     <script>
         document.querySelectorAll('.bank-option').forEach(function(element) {
             element.addEventListener('click', function() {
+                document.querySelectorAll('.bank-option').forEach(function(el) {
+                    el.classList.remove('active');
+                });
+                this.classList.add('active');
+
                 var bankName = this.getAttribute('data-bank');
                 var bankImage = this.getAttribute('data-image');
                 localStorage.setItem('selectedBank', bankName);
                 localStorage.setItem('selectedBankImage', bankImage);
+                document.querySelector('#pay-now').setAttribute('href', '/VirtualAccountUser?bank=' + bankName);
             });
         });
 
